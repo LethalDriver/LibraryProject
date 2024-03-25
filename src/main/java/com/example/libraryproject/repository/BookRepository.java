ec0ac463f6eedfcf90246a8677d668f19dc2d64f
mwdziak <mwdziak@gmail.com>
2024-03-25 10:37:38 +0100

    Book repository now also extends crud repository

--- src/main/java/com/example/libraryproject/repository/BookRepository.java
+++ src/main/java/com/example/libraryproject/repository/BookRepository.java
@@ -1,10 +1,11 @@
 package com.example.libraryproject.repository;
 
 import com.example.libraryproject.domain.Book;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
+public interface BookRepository extends PagingAndSortingRepository<Book, Long>, CrudRepository<Book, Long>{
     List<Book> findByTitleContainingIgnoreCase(String title);
 }
