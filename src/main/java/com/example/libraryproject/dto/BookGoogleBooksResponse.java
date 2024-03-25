f51161e894787984504647a649d947b4299cf459
mwdziak <mwdziak@gmail.com>
2024-03-25 10:21:56 +0100

    Google Books response DTO class deleted

--- src/main/java/com/example/libraryproject/dto/BookGoogleBooksResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.libraryproject.dto;
-
-public class BookGoogleBooksResponse {
-    private String title;
-    private String author;
-    private String description;
-    private String isbn;
-    private String publisher;
-}
