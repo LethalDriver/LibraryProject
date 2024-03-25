099474b7f5b5100fae89f665c487527cc2847dfc
mwdziak <mwdziak@gmail.com>
2024-03-25 10:08:44 +0100

    Book repository defined

---
+++ src/main/java/com/example/libraryproject/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.example.libraryproject.repository;
+
+import com.example.libraryproject.domain.Book;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+
+public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
+    List<Book> findByTitleContainingIgnoreCase(String title);
+}
