30998afe5349a07e492290194cad51af80fdc614
mwdziak <mwdziak@gmail.com>
2024-03-25 10:16:24 +0100

    Google Books response DTO class defined

---
+++ src/main/java/com/example/libraryproject/dto/BookGoogleBooksResponse.java
@@ -0,0 +1,9 @@
+package com.example.libraryproject.dto;
+
+public class BookGoogleBooksResponse {
+    private String title;
+    private String author;
+    private String description;
+    private String isbn;
+    private String publisher;
+}
