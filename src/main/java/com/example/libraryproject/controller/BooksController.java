9ec76e361ba7071d7847af5c96f230b2166c387d
mwdziak <mwdziak@gmail.com>
2024-03-26 13:18:02 +0100

    Books controller defined, DTO for book class yet to be implemented

---
+++ src/main/java/com/example/libraryproject/controller/BooksController.java
@@ -0,0 +1,21 @@
+package com.example.libraryproject.controller;
+
+import com.example.libraryproject.domain.Book;
+import com.example.libraryproject.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController("/books")
+public class BooksController {
+    private final BookService bookService;
+    @GetMapping
+    public ResponseEntity<List<Book>> getBooksMatchingTitle(@RequestParam String title) {
+        return ResponseEntity.ok(bookService.getBooks(title));
+    }
+}
