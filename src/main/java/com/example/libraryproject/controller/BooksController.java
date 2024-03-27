cdb5a824d1b8316da471e7a6c2eeab0ae330b0ac
mwdziak <mwdziak@gmail.com>
2024-03-27 13:46:24 +0100

    Book controller now returns DTOs instead of entities

--- src/main/java/com/example/libraryproject/controller/BooksController.java
+++ src/main/java/com/example/libraryproject/controller/BooksController.java
@@ -1,6 +1,7 @@
 package com.example.libraryproject.controller;
 
 import com.example.libraryproject.domain.Book;
+import com.example.libraryproject.dto.BookDTO;
 import com.example.libraryproject.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,13 +17,13 @@ import java.util.Optional;
 public class BooksController {
     private final BookService bookService;
     @GetMapping
-    public ResponseEntity<List<Book>> getBooksMatchingTitle(@RequestParam String title) {
+    public ResponseEntity<List<BookDTO>> getBooksMatchingTitle(@RequestParam String title) {
         return ResponseEntity.ok(bookService.getAllBooksMatchingTitle(title));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Book> getBookById(@PathVariable String id) {
-        Optional<Book> book = bookService.getBookById(Long.parseLong(id));
+    public ResponseEntity<BookDTO> getBookById(@PathVariable String id) {
+        Optional<BookDTO> book = bookService.getBookById(Long.parseLong(id));
         return book.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 }
--- src/main/java/com/example/libraryproject/service/BookService.java
+++ src/main/java/com/example/libraryproject/service/BookService.java
@@ -1,6 +1,8 @@
 package com.example.libraryproject.service;
 
 import com.example.libraryproject.domain.Book;
+import com.example.libraryproject.dto.BookDTO;
+import com.example.libraryproject.mapper.BookMapper;
 import com.example.libraryproject.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,21 +15,22 @@ import java.util.Optional;
 public class BookService {
     private final BookRepository bookRepository;
     private final GoogleBooksService googleBooksService;
+    private final BookMapper bookMapper;
     private List<Book> findBookByTitleInRepository (String title) {
         return bookRepository.findByTitleContainingIgnoreCase(title);
     }
 
-    public List<Book> getAllBooksMatchingTitle (String title) {
+    public List<BookDTO> getAllBooksMatchingTitle (String title) {
         List<Book> resultsFromRepo = findBookByTitleInRepository(title);
         if (resultsFromRepo.isEmpty()) {
             List<Book> resultsFromGoogle = googleBooksService.getBooks(title);
             bookRepository.saveAll(resultsFromGoogle);
-            return resultsFromGoogle;
+            return resultsFromGoogle.stream().map(bookMapper::toDto).toList();
         }
-        return resultsFromRepo;
+        return resultsFromRepo.stream().map(bookMapper::toDto).toList();
     }
 
-    public Optional<Book> getBookById (Long id) {
-        return bookRepository.findById(id);
+    public Optional<BookDTO> getBookById (Long id) {
+        return bookRepository.findById(id).map(bookMapper::toDto);
     }
 }
