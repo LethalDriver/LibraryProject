49b6304cfff9770347005278f42341e8fab894d0
mwdziak <mwdziak@gmail.com>
2024-03-27 13:28:43 +0100

    Book controller fixed by adding a request mapping annotation

--- src/main/java/com/example/libraryproject/controller/BooksController.java
+++ src/main/java/com/example/libraryproject/controller/BooksController.java
@@ -5,6 +5,7 @@ import com.example.libraryproject.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,8 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController("/books")
+@RestController
+@RequestMapping("/books")
 public class BooksController {
     private final BookService bookService;
     @GetMapping
