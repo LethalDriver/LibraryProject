6ebb0a4079108e648239366f9a6bf38c00981256
mwdziak <mwdziak@gmail.com>
2024-03-27 15:27:12 +0100

    Loan repository defined with necessary methods

---
+++ src/main/java/com/example/libraryproject/repository/LoanRepository.java
@@ -0,0 +1,13 @@
+package com.example.libraryproject.repository;
+
+import com.example.libraryproject.domain.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+    List<Loan> findByStatus(Loan.Status status);
+    List<Loan> findByReturnDateBeforeAndStatusNot(LocalDate date, Loan.Status status);
+    List<Loan> findByUserId(Long userId);
+}
