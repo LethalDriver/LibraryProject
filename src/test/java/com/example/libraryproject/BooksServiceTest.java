6a703680bab3d751ac18ad107ce07f3571ca054f
mwdziak <mwdziak@gmail.com>
2024-03-27 13:14:53 +0100

    Book service test implemented

---
+++ src/test/java/com/example/libraryproject/BooksServiceTest.java
@@ -0,0 +1,59 @@
+package com.example.libraryproject;
+
+import com.example.libraryproject.domain.Book;
+import com.example.libraryproject.repository.BookRepository;
+import com.example.libraryproject.service.BookService;
+import com.example.libraryproject.service.GoogleBooksService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class BooksServiceTest {
+    @Mock
+    private BookRepository bookRepository;
+
+    @Mock
+    private GoogleBooksService googleBooksService;
+
+    @InjectMocks
+    private BookService bookService;
+
+    @Test
+    public void testGetBooksFromRepository() {
+        String title = "Harry Potter";
+        Book book = new Book();
+        book.setTitle(title);
+        when(bookRepository.findByTitleContainingIgnoreCase(eq(title))).thenReturn(Arrays.asList(book));
+
+        List<Book> books = bookService.getBooks(title);
+
+        assertEquals(1, books.size());
+        assertEquals(title, books.get(0).getTitle());
+    }
+
+    @Test
+    public void testGetBooksFromGoogleBooksService() {
+        String title = "Harry Potter";
+        Book book = new Book();
+        book.setTitle(title);
+        when(bookRepository.findByTitleContainingIgnoreCase(eq(title))).thenReturn(Collections.emptyList());
+        when(googleBooksService.getBooks(eq(title))).thenReturn(List.of(book));
+
+        List<Book> books = bookService.getBooks(title);
+
+        assertEquals(1, books.size());
+        assertEquals(title, books.get(0).getTitle());
+    }
+
+}
