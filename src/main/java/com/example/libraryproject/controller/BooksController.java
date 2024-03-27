25e1731d55cb2f965a1e7127c661f0c5453f4ed2
mwdziak <mwdziak@gmail.com>
2024-03-27 13:36:47 +0100

    Mapping for finding the book by id added to book controller

--- src/main/java/com/example/libraryproject/controller/BooksController.java
+++ src/main/java/com/example/libraryproject/controller/BooksController.java
@@ -3,13 +3,12 @@ package com.example.libraryproject.controller;
 import com.example.libraryproject.domain.Book;
 import com.example.libraryproject.service.BookService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,5 +18,11 @@ public class BooksController {
     @GetMapping
     public ResponseEntity<List<Book>> getBooksMatchingTitle(@RequestParam String title) {
         return ResponseEntity.ok(bookService.getAllBooksMatchingTitle(title));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable String id) {
+        Optional<Book> book = bookService.getBookById(Long.parseLong(id));
+        return book.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 }
