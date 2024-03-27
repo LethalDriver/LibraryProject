dfe486baf579b591b005ed0fb9b87aae5145e059
mwdziak <mwdziak@gmail.com>
2024-03-27 13:40:12 +0100

    Book DTO and mapper defined

---
+++ src/main/java/com/example/libraryproject/dto/BookDTO.java
@@ -0,0 +1,11 @@
+package com.example.libraryproject.dto;
+
+public record BookDTO(
+        Long id,
+        String title,
+        String author,
+        String isbn,
+        String publisher,
+        Integer availableCopies
+) {
+}
---
+++ src/main/java/com/example/libraryproject/mapper/BookMapper.java
@@ -0,0 +1,12 @@
+package com.example.libraryproject.mapper;
+
+import com.example.libraryproject.domain.Book;
+import com.example.libraryproject.dto.BookDTO;
+import org.mapstruct.Builder;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring", builder = @Builder(disableBuilder = true))
+public interface BookMapper {
+    BookDTO toDto(Book book);
+    Book toEntity(BookDTO bookDto);
+}
