4af0b76b9784abce504bcd1dff619ed44bd49afd
mwdziak <mwdziak@gmail.com>
2024-03-30 20:39:11 +0100

    Added "/request" path mapping to loan requesting endpoint.

--- src/main/java/com/example/libraryproject/controller/LoanController.java
+++ src/main/java/com/example/libraryproject/controller/LoanController.java
@@ -31,7 +31,7 @@ public class LoanController {
         return ResponseEntity.ok(loan);
     }
 
-    @PostMapping
+    @PostMapping("/request")
     public ResponseEntity<LoanDTO> requestLoan(@RequestParam Long bookId) {
         Long currentUserId = userService.getCurrentUser().getId();
         LoanDTO loanDTO = loanService.requestBookLoan(bookId, currentUserId);
