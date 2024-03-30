e01be3d24425ce38bef9c1cacb9e67cf0ff8a410
mwdziak <mwdziak@gmail.com>
2024-03-30 21:05:05 +0100

    ReviewDate field added to review entity and DTO.

--- src/main/java/com/example/libraryproject/domain/Review.java
+++ src/main/java/com/example/libraryproject/domain/Review.java
@@ -9,6 +9,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Data
 @Entity
 @Builder
@@ -19,6 +21,7 @@ public class Review {
     private Long id;
     private String review;
     private Integer rating;
+    private LocalDate reviewDate;
     @ManyToOne(cascade = CascadeType.ALL)
     private Book book;
     @ManyToOne(cascade = CascadeType.ALL)
