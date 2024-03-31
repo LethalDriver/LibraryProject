da3715dd3fb6f942e99cd26e52238bc55affee52
mwdziak <mwdziak@gmail.com>
2024-03-31 13:09:25 +0200

    Added custom mapping to allow to map nested book object.

--- src/main/java/com/example/libraryproject/mapper/BookMapper.java
+++ src/main/java/com/example/libraryproject/mapper/BookMapper.java
@@ -4,9 +4,12 @@ import com.example.libraryproject.domain.Book;
 import com.example.libraryproject.dto.BookDTO;
 import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", builder = @Builder(disableBuilder = true), uses = {BookDetailsMapper.class})
 public interface BookMapper {
+    @Mapping(target = "bookDetails", source = "bookDetails")
     BookDTO toDto(Book book);
+    @Mapping(target = "bookDetails", source = "bookDetails")
     Book toEntity(BookDTO bookDto);
 }
