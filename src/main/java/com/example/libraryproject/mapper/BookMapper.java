b8b7b35ca8dad42c2b6982999743ab1c2ed398eb
mwdziak <mwdziak@gmail.com>
2024-03-31 12:46:39 +0200

    BookMapper now uses BookDetails mapper.

--- src/main/java/com/example/libraryproject/mapper/BookMapper.java
+++ src/main/java/com/example/libraryproject/mapper/BookMapper.java
@@ -5,7 +5,7 @@ import com.example.libraryproject.dto.BookDTO;
 import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring", builder = @Builder(disableBuilder = true))
+@Mapper(componentModel = "spring", builder = @Builder(disableBuilder = true), uses = {BookDetailsMapper.class})
 public interface BookMapper {
     BookDTO toDto(Book book);
     Book toEntity(BookDTO bookDto);
