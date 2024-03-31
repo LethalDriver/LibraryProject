6004a0b45c4f582dbc3ecf0364d7a371d688198e
mwdziak <mwdziak@gmail.com>
2024-03-31 13:04:35 +0200

    Specified column size of summary field in BookDetails.

--- src/main/java/com/example/libraryproject/domain/BookDetails.java
+++ src/main/java/com/example/libraryproject/domain/BookDetails.java
@@ -1,9 +1,6 @@
 package com.example.libraryproject.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,6 +16,7 @@ public class BookDetails {
     @GeneratedValue
     private Long id;
     private String genre;
+    @Column(length = 2000)
     private String summary;
     private String coverImageUrl;
     @OneToOne
