5222b4359873f36bc6f6ce4ecd4548bccf5c5297
mwdziak <mwdziak@gmail.com>
2024-04-05 09:35:00 +0200

    Removed redundant mappings from loan mapper.

--- src/main/java/com/example/libraryproject/mapper/LoanMapper.java
+++ src/main/java/com/example/libraryproject/mapper/LoanMapper.java
@@ -9,15 +9,11 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring", builder = @Builder(disableBuilder = true), uses = {BookMapper.class, UserMapper.class})
 public interface LoanMapper {
 
-    @Mapping(target = "bookId", source = "book.id")
-    @Mapping(target = "userId", source = "user.id")
     @Mapping(target = "loanDate", source = "loanDate", dateFormat = "yyyy-MM-dd")
     @Mapping(target = "returnDate", source = "returnDate", dateFormat = "yyyy-MM-dd")
     @Mapping(target = "dueDate", source = "dueDate", dateFormat = "yyyy-MM-dd")
     LoanDTO toDTO(Loan loan);
 
-    @Mapping(target = "book.id", source = "bookId")
-    @Mapping(target = "user.id", source = "userId")
     @Mapping(target = "loanDate", source = "loanDate", dateFormat = "yyyy-MM-dd")
     @Mapping(target = "returnDate", source = "returnDate", dateFormat = "yyyy-MM-dd")
     @Mapping(target = "dueDate", source = "dueDate", dateFormat = "yyyy-MM-dd")
