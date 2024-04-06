5908564520c753fc46178c1a4ea0363c5f606017
mwdziak <mwdziak@gmail.com>
2024-04-06 16:30:13 +0200

    Review is now correctly saved with book and user foreign keys.

--- src/main/java/com/example/libraryproject/service/ReviewService.java
+++ src/main/java/com/example/libraryproject/service/ReviewService.java
@@ -1,14 +1,19 @@
 package com.example.libraryproject.service;
 
+import com.example.libraryproject.domain.Book;
 import com.example.libraryproject.domain.User;
 import com.example.libraryproject.dto.ReviewDTO;
 import com.example.libraryproject.dto.ReviewPostRequest;
 import com.example.libraryproject.mapper.ReviewMapper;
+import com.example.libraryproject.repository.BookRepository;
 import com.example.libraryproject.repository.ReviewRepository;
+import com.example.libraryproject.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 
@@ -17,6 +22,8 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class ReviewService {
     private final ReviewRepository reviewRepository;
+    private final BookRepository bookRepository;
+    private final UserRepository userRepository;
     private final ReviewMapper reviewMapper;
     private final UserService userService;
 
@@ -37,7 +44,20 @@ public class ReviewService {
         if (!Objects.equals(reviewPostRequest.userId(), currentUserId)) {
             throw new IllegalStateException("User with id " + reviewPostRequest.userId() + " cannot add review for user with id " + currentUserId);
         }
-        var review = reviewRepository.save(reviewMapper.toEntity(reviewPostRequest));
+
+        Book book = bookRepository.findById(reviewPostRequest.bookId())
+                .orElseThrow(() -> new EntityNotFoundException("Book not found"));
+
+        User user = userRepository.findById(reviewPostRequest.userId())
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+
+
+        var review = reviewMapper.toEntity(reviewPostRequest);
+        review.setBook(book);
+        review.setUser(user);
+        review.setReviewDate(LocalDate.now());
+        review = reviewRepository.save(review);
+
         return reviewMapper.toDTO(review);
     }
 
