0bdaf0cbdd5b8f717b5384388e59a41cf5956904
mwdziak <mwdziak@gmail.com>
2024-05-15 12:12:29 +0200

    Updated also the reviewPost request

--- src/main/java/com/example/libraryproject/dto/ReviewPostRequest.java
+++ src/main/java/com/example/libraryproject/dto/ReviewPostRequest.java
@@ -2,7 +2,7 @@ package com.example.libraryproject.dto;
 
 public record ReviewPostRequest(
         Long bookId,
-        String review,
+        String reviewContent,
         Integer rating
 ) {
 }
